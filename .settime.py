459fb11eef2482593d2a1eb058f5f0ac5507db14
Blayung <gpewojan1@gmail.com>
2022-10-23 10:00:39 +0200

    Update .settime.py

--- .settime.py
+++ .settime.py
@@ -6,7 +6,7 @@ from os import system
 key='' #API key
 location='Warsaw, Poland' #Format: "City, Country"
 try:
-    time=loads(get(f'https://timezone.abstractapi.com/v1/current_time/?api_key={key}&location={location}').text)['datetime'] #To save amount of requests
+    time=loads(get(f'https://timezone.abstractapi.com/v1/current_time/?api_key={key}&location={location}').text)['datetime']
 except:
     print('error occured while requesting the time from https://timezone.apstractapi.com')
     exit()
