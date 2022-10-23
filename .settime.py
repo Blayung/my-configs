9a1a617f8d7bf58629d7fc76c2f116e822a8e6f0
Blayung <gpewojan1@gmail.com>
2022-10-23 09:59:29 +0200

    Add .settime.py

---
+++ .settime.py
@@ -0,0 +1,15 @@
+#!/bin/python3
+from json import loads
+from requests import get
+from os import system
+
+key='' #API key
+location='Warsaw, Poland' #Format: "City, Country"
+try:
+    time=loads(get(f'https://timezone.abstractapi.com/v1/current_time/?api_key={key}&location={location}').text)['datetime'] #To save amount of requests
+except:
+    print('error occured while requesting the time from https://timezone.apstractapi.com')
+    exit()
+
+print(f'timedatectl set-time "{time}"')
+system(f'timedatectl set-time "{time}"')
